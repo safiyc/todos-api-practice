c793c58335e1ad93a3c8bb7b3357e84e5260855a
safiy cham <csafi13@yahoo.com>
2017-12-13 10:29:06 -0800

    add spec tests to validate presence and association of Todo and Item.

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ApplicationRecord
+  # 'dependent: :destroy' when the object is destroyed, destroy will be called on its associated objects.
+  has_many :items, dependent: :destroy
+
+  validates_presence_of :title, :created_by
 end
